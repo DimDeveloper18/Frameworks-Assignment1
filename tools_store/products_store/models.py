a9500107d20f42412c00a0d5fba8afd2a470249d
DimDeveloper18 <dimash.itcout@gmail.com>
2025-11-19 17:21:55 +0000

    Django libraries added for 'timezone', 'get_user_model', 'User', 'Comment' models created and 'str' function added.

--- tools_store/products_store/models.py
+++ tools_store/products_store/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your models here.
+
+class Comment(models.Model):
+    comwriter = models.ForeignKey(User, on_delete=models.CASCADE)
+    comname = models.CharField(max_length=100)
+    comtext = models.TextField()
+    commented_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.comname
