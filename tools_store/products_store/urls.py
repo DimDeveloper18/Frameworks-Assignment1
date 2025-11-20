1edb42b3b9f3924b1a315eaf76975e3fcaad7f24
DimDeveloper18 <dimash.itcout@gmail.com>
2025-11-20 12:42:22 +0000

    Path created for 'comments_page'.

--- tools_store/products_store/urls.py
+++ tools_store/products_store/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='products_store/login.html'), name='products_store-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='products_store/logout.html'), name='products_store-logout'),
     path('basket/', views.basket_page, name='products_store-basket_page'),
+    path('comments_view/', views.comments_view, name='custom_comments/comments-view'),
     path('profile/', views.profile_page, name='products_store-profile_page'),
 ]
