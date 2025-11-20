e099d9e95b01c3e83914d7519cfb91fc676df3b6
DimDeveloper18 <dimash.itcout@gmail.com>
2025-11-20 11:28:56 +0000

    Paths created for 'logout', 'basket', 'profile', pages.

--- tools_store/products_store/urls.py
+++ tools_store/products_store/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('delivery/', views.delivery, name='products_store-delivery'),
     path('register/', views.reg_form, name='products_store-reg_form'),
     path('login/', auth_views.LoginView.as_view(template_name='products_store/login.html'), name='products_store-login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='products_store/logout.html'), name='products_store-logout'),
+    path('basket/', views.basket_page, name='products_store-basket_page'),
+    path('profile/', views.profile_page, name='products_store-profile_page'),
 ]
