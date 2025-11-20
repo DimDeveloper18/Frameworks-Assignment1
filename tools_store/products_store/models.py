0bc1dc859db07c1e453b92b4c256f3e9c771ec7b
DimDeveloper18 <dimash.itcout@gmail.com>
2025-11-20 13:12:16 +0000

    Class created 'User_profile' and functions 'str' and 'save' with 'if' statement for profile images resize. Django libraries added for 'User' and 'Image'.

--- tools_store/products_store/models.py
+++ tools_store/products_store/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from PIL import Image
 
 User = get_user_model()
 
@@ -14,3 +16,21 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.comname
+    
+
+class User_profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} User_profile'
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        prof_img = Image.open(self.image.path)
+
+        if prof_img.height > 70 or prof_img.width > 70:
+            resize = (70, 70)
+            prof_img.thumbnail(resize)
+            prof_img.save(self.image.path)
