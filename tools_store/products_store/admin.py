307ab891be35804c300e1f3eac3bad80831be131
DimDeveloper18 <dimash.itcout@gmail.com>
2025-11-20 13:19:26 +0000

    Django libraries added for 'Comment' and 'User_profile' also register models created for them.

--- tools_store/products_store/admin.py
+++ tools_store/products_store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from. models import Comment
+from .models import User_profile
 
 # Register your models here.
+
+admin.site.register(Comment)
+admin.site.register(User_profile)
