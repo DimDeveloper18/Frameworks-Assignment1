6334fdd1c6c0b20a3949a21e3f4dba5ea0fca008
DimDeveloper18 <dimash.itcout@gmail.com>
2025-11-26 10:17:17 +0000

    Comment variable 'comwriter' param. (null) added. Function created 'get_absolute_url' and 'reverse' library added for created comment.

--- tools_store/products_store/models.py
+++ tools_store/products_store/models.py
@@ -3,19 +3,23 @@ from django.utils import timezone
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from PIL import Image
+from django.urls import reverse
 
 User = get_user_model()
 
 # Create your models here.
 
 class Comment(models.Model):
-    comwriter = models.ForeignKey(User, on_delete=models.CASCADE)
+    comwriter = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     comname = models.CharField(max_length=100)
     comtext = models.TextField()
     commented_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.comname
+    
+    def get_absolute_url(self):
+        return reverse('products_store-comment-detail', kwargs={"pk": self.pk})
     
 
 class User_profile(models.Model):
