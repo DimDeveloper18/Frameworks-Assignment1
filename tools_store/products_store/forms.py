e5f522de9063818f7750b4ab0d17439d2f5974f2
DimDeveloper18 <dimash.itcout@gmail.com>
2025-11-26 10:06:51 +0000

    Classes created for (UserUpdateDetailsForm, User_profileUpdateForm, CommentForm), and libraries added for (User_profile and Comment).

--- tools_store/products_store/forms.py
+++ tools_store/products_store/forms.py
@@ -3,7 +3,8 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
-# from .models import User_profile
+from .models import User_profile
+from .models import Comment
 
 User = get_user_model()
 
@@ -20,3 +21,27 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateDetailsForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class User_profileUpdateForm(forms.ModelForm):
+    
+    class Meta:
+        model = User_profile
+        fields = ['image']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comname', 'comtext']
+        labels = {
+            "comname": "Name", "comtext": "Comment",
+        }
+        widgets = {
+            "comtext": forms.Textarea(attrs={"rows": 3})
+        }
