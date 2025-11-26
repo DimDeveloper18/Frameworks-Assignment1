b7c63f82d53a35d690b75afa7bbc4b6f30038c20
DimDeveloper18 <dimash.itcout@gmail.com>
2025-11-26 09:55:39 +0000

    Classes created for 'CommentsList', 'CommentsDetail', 'CommentsCreate', and function for 'comment_form'. Added variable 'comments' to function 'tools'. Labraries added for (UserUpdateDetailsForm, User_profileUpdateForm, ListView, DetailView, CreateView, CommentForm).

--- tools_store/products_store/views.py
+++ tools_store/products_store/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateDetailsForm, User_profileUpdateForm
 from django.contrib import messages
 from .models import Comment
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from .forms import CommentForm
 
 
 
@@ -12,7 +15,11 @@ def index(request):
     return render(request, 'products_store/index.html')
 
 def tools(request):
-    return render(request, 'products_store/tools.html')
+    comments = {
+        'comments': Comment.objects.all(),
+    }
+    # print(comments)
+    return render(request, 'products_store/tools.html', comments)
 
 def contact(request):
     return render(request, 'products_store/contact.html')
@@ -22,9 +29,28 @@ def delivery(request):
 
 def comments_view(request):
     com_consist = {
-        'comments': Comment.objects.all()
+        'comments': Comment.objects.all(),
     }
     return render(request, 'products_store/comments_page.html', com_consist)
+
+class CommentsList(ListView):
+    model = Comment
+    template_name = 'products_store/tools.html'
+    com_consist_object_name = 'comments'
+    com_sorting = ['-commented_date']
+
+class CommentsDetail(DetailView):
+    model = Comment
+
+class CommentsCreate(CreateView):
+    model = Comment
+    fields = ['comname', 'comtext']
+    # form_class = CommentForm
+    # template_name = 'products_store/comment_form.html'
+
+    def comment_form(self, form):
+        form.instance.comwriter = self.request.user
+        return super().comment_form(form)
 
 def reg_form(request):
     if request.method == 'POST':
@@ -53,7 +79,7 @@ def profile_page(request):
             uud_form.save()
             upu_form.save()
             messages.success(request, f'User details successfuly updated!')
-            return redirect('customers-profile_page')
+            return redirect('products_store-profile_page')
     else:
         uud_form = UserUpdateDetailsForm(instance=request.user)
         upu_form = User_profileUpdateForm(instance=request.user.user_profile)
@@ -64,4 +90,4 @@ def profile_page(request):
         'uud_form': uud_form,
         'upu_form': upu_form
     }
-    return render(request, 'customers/profile.html', content)
+    return render(request, 'products_store/profile.html', content)
