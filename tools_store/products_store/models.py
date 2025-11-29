792a4d753e697520e1a32648523eb39f19ce7c17
DimDeveloper18 <dimash.itcout@gmail.com>
2025-11-29 19:45:59 +0000

    Cloudinary settings added to 'User_profile' class and to library.

--- tools_store/products_store/models.py
+++ tools_store/products_store/models.py
@@ -2,18 +2,18 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
-from PIL import Image
 from django.urls import reverse
+from cloudinary_storage.storage import MediaCloudinaryStorage
 
 User = get_user_model()
 
 # Create your models here.
 
 class Comment(models.Model):
-    comwriter = models.ForeignKey(User, on_delete=models.CASCADE)
     comname = models.CharField(max_length=100)
     comtext = models.TextField()
     commented_date = models.DateTimeField(default=timezone.now)
+    comwriter = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.comname
@@ -24,17 +24,8 @@ class Comment(models.Model):
 
 class User_profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(storage=MediaCloudinaryStorage(), upload_to='profile_pics/')
 
     def __str__(self):
         return f'{self.user.username} User_profile'
     
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        prof_img = Image.open(self.image.path)
-
-        if prof_img.height > 70 or prof_img.width > 70:
-            resize = (70, 70)
-            prof_img.thumbnail(resize)
-            prof_img.save(self.image.path)
